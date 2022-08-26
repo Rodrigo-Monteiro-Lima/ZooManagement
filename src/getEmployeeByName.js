2288ea618ca8117b251d32fa968fc296e71a4694
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 15:29:16 -0300

    terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find((employee) =>
+    employeeName === employee.firstName || employeeName === employee.lastName);
 }
 
 module.exports = getEmployeeByName;
