b70d8e6328fad9480f0234402e16de2ccfa201c8
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 16:55:27 -0300

    décimo requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findResponsible = employees.find((employee) => id === employee.id).responsibleFor[0];
+  const findResidents = species.find((animal) => animal.id.includes(findResponsible)).residents
+    .sort((a, b) => b.age - a.age);
+  const olderAnimal = findResidents[0];
+  const { name, sex, age } = olderAnimal;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
