bc9d03009614bba90b5abb27dc0b0a93151aa234
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 15:33:37 -0300

    quarto requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    return employees.filter((employee) => employee.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
