90d124bee07daa98233940e4cd37d6ade947b55b
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 14:58:39 -0300

    primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+const { species } = data;
+
+const compareIds = (animal, ids) => {
+  for (let index = 0; index < ids.length; index += 1) {
+    if (animal.id === ids[index]) {
+      return true;
+    }
+  }
+};
+
+function getSpeciesByIds(...ids) {
+  return species.filter((animal) => compareIds(animal, ids));
 }
 
 module.exports = getSpeciesByIds;
