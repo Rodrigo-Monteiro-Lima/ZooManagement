27dd5b5795b093ed1ea01717d5c2b2f2382a40d6
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 15:42:02 -0300

    quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getAllAnimals = (acc, curr) => {
+  const findSpecie = species.find((animal) => animal.name === curr.name);
+  acc[curr.name] = findSpecie.residents.length;
+  return acc;
+};
+
+const getAnimal = ({ specie, sex }) => {
+  if (!sex) {
+    const findSpecie = species.find((beast) => beast.name === specie);
+    const residentsNumber = findSpecie.residents.length;
+    return residentsNumber;
+  } const findSpecie = species.find((beast) => beast.name === specie);
+  const residentsNumber = findSpecie.residents.filter((beast) => beast.sex === sex).length;
+  return residentsNumber;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curr) => getAllAnimals(acc, curr), {});
+  } return getAnimal(animal);
 }
 
 module.exports = countAnimals;
