fde1da0635f9cdf64aac8af8304c950a614c1de0
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 16:17:01 -0300

    oitavo requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = {
+    child: entrants.filter((person) => person.age < 18).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    senior: entrants.filter((person) => person.age >= 50).length,
+  };
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Array.isArray(entrants) === false) return 0;
+  const peopleAmount = countEntrants(entrants);
+  const { child, adult, senior } = peopleAmount;
+  const { adult: adultPrice, senior: seniorPrice, child: childPrice } = prices;
+  return (child * childPrice) + (adult * adultPrice) + (senior * seniorPrice);
 }
 
 module.exports = { calculateEntry, countEntrants };
