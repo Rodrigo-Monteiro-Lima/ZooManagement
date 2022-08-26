488a463d641ce5b94791f46e146d82f14ec14ee8
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-26 15:06:11 -0300

    segundo requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => specie.name === animal)
+    .residents.every((specie) => specie.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
